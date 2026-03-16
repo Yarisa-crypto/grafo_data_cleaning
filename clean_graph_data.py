4bb1044196c2c13f2a3512d213c5e146a9c3a9eb
Yefri <y.arisa@utp.edu.co>
2026-03-16 13:23:18 -0500

    feat: initial graph data cleaning script

---
+++ clean_graph_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import networkx as nx
+
+def clean_graph(file):
+    df = pd.read_csv(file)
+
+    #Eliminar valores nulos
+    df = df.dropna()
+
+    #Eliminar duplicados
+    df = df.drop_duplicates()
+
+    return df
+
+if __name__ == "__main__":
+    data = clean_graph("data.csv")
+    print(data.head())
